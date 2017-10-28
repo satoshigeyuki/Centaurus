8ff1c2513e34ba64e91e9757db4f25381b7649d1
Hiroka Ihara <ihara@eidos.ic.i.u-tokyo.ac.jp>
2017-10-28 18:53:12 +0900

    Implemented CppAlienCode class

--- grammar.hpp
+++ grammar.hpp
@@ -5,13 +5,9 @@
 
 namespace Centaurus
 {
-std::wistream::int_type skip_chars(std::wistream& stream)
-{
-    std::wistream::int_type ch = stream.get();
-    //for (; ch == L' ' || ch == L'\t' || ch == '\r' || 
-}
 class AlienCode
 {
+    std::wstring m_str;
 public:
     AlienCode()
     {
@@ -22,19 +18,91 @@ public:
 };
 class CppAlienCode : public AlienCode
 {
-    void parse_multiline_comment(WideSentryStream& stream)
+    using AlienCode::m_str;
+
+    void parse_multiline_comment(WideStream& stream)
     {
+        wchar_t ch = stream.get();
+        for (; ch != L'\0'; ch = stream.get())
+        {
+            if (ch == L'*')
+            {
+                ch = stream.get();
+                if (ch == L'/')
+                {
+                    return;
+                }
+            }
+        }
+        throw stream.unexpected(EOF);
     }
-    void parse_oneline_comment(WideSentryStream& stream)
+    void parse_oneline_comment(WideStream& stream)
     {
+        wchar_t ch = stream.get();
+        for (; ch != L'\0'; ch = stream.get())
+        {
+            if (ch == L'\n')
+                return;
+            if (ch == L'\r')
+            {
+                ch = stream.get();
+                if (ch == L'\n')
+                    return;
+            }
+        }
+        throw stream.unexpected(EOF);
     }
-    void parse(WideSentryStream& stream)
+    void parse_literal_string(WideStream& stream)
     {
+        wchar_t ch = stream.get();
+        for (; ch != L'\0'; ch = stream.get())
+        {
+            if (ch == L'"')
+                return;
+        }
+        throw stream.unexpected(EOF);
+    }
+    void parse_literal_character(WideStream& stream)
+    {
+        wchar_t ch = stream.get();
+        if (ch == '\\')
+            ch = stream.get();
+        ch = stream.get();
+        if (ch != '\'')
+            throw stream.unexpected(ch);
+    }
+    void parse(WideStream& stream)
+    {
+        while (true)
+        {
+            wchar_t ch = stream.skip_whitespace();
+
+            switch (ch)
+            {
+            case L'/':
+                ch = stream.get();
+                if (ch == L'/')
+                    parse_oneline_comment(stream);
+                else if (ch == L'*')
+                    parse_multiline_comment(stream);
+                break;
+            case L'"':
+                parse_literal_string(stream);
+                break;
+            case L'\'':
+                parse_literal_character(stream);
+                break;
+            }
+        }
     }
 public:
-    CppAlienCode(WideSentryStream& stream)
+    CppAlienCode(WideStream& stream)
     {
+        WideStream::Sentry begin = stream.sentry();
+
         parse(stream);
+
+        m_str = stream.cut(begin);
     }
     virtual ~CppAlienCode()
     {
@@ -47,8 +115,9 @@ class Grammar
 {
     std::list<Production> m_productions;
 public:
-    Grammar(std::wistream& stream)
+    Grammar(WideStream& stream)
     {
+
     }
     ~Grammar()
     {
