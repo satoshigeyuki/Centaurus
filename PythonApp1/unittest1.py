fdd59eaa07ec7e068d5f39186ad38b7d5f39e7ef
Hiroka Ihara <ihara@eidos.ic.i.u-tokyo.ac.jp>
2018-07-23 13:48:20 +0900

    Grammar printing works

--- PythonApp1/unittest1.py
+++ PythonApp1/unittest1.py
@@ -6,7 +6,8 @@ class Test_unittest1(unittest.TestCase):
     def test_corelib_load(self):
         corelib = CoreLib.get_instance()
     def test_grammar(self):
-        grammar = Grammar.Grammar()
+        grammar = Grammar.Grammar(r"..\grammar\json.cgr")
+        grammar.print(r"..\grammar\json.dot")
 
 if __name__ == '__main__':
     unittest.main()
