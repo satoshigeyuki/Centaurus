7579769f6e0b0d661b6c36dd3ed8279c065efb29
Hiroka IHARA <ihara@eidos.ic.i.u-tokyo.ac.jp>
2018-10-20 23:54:03 +0900

    Added dry-run benchmark written in Python

---
+++ PythonApp1/drybench.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+from __future__ import print_function
+import time
+import os
+
+os.environ['CENTAURUS_DL_PATH'] = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "build")
+from Centaurus import *
+
+if __name__ == "__main__":
+    grammar = Grammar(r"../grammar/json2.cgr")
+    parser = Parser(grammar, True)
+    input_path = r"/home/ihara/Downloads/sf-city-lots-json-master/citylots.json"
+    st1_runner = Stage1Runner(input_path, parser, 8 * 1024 * 1024, 8);
+    start_time = time.time()
+    st1_runner.start()
+    print("Runner started.", flush=True)
+    st1_runner.wait()
+    print("Runner finished.", flush=True)
+    end_time = time.time()
+    print("Elapsed time = %lf" % ((end_time - start_time) * 1E+3,))
