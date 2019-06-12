eecd7de71dd0f1569b133199698a57ef6de1d998
SATO Shigeyuki <sato@eidos.ic.i.u-tokyo.ac.jp>
2019-06-13 01:41:28 +0900

    refactor benchmarks in Python.

--- PythonApp1/bench2.py
+++ PythonApp1/bench2.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 
 from __future__ import print_function
+import sys
 import os
 import logging
 import multiprocessing as mp
 import time
 
-os.environ['CENTAURUS_DL_PATH'] = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "build")
+os.environ['CENTAURUS_DL_PATH'] = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'build')
 
 from Centaurus import *
 
@@ -14,71 +15,52 @@ logger = getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
 class JsonListener(object):
-    def __init__(self):
-        self.count = 0
     def parseNumber(self, ctx):
         try:
             return float(ctx.read().strip())
         except ValueError:
             return 0.0
     def parseString(self, ctx):
-        #return ctx.read().decode('utf-8')[1:-1]
         return ctx.read().strip()[1:-1]
     def parseBoolean(self, ctx):
-        #return ctx.read().decode('utf-8').lower() == 'true'
         return ctx.read().strip().lower() == 'true'
     def parseNone(self, ctx):
-        return None
+        pass
     def parseObject(self, ctx):
-        return ctx[0] if ctx else None
+        if ctx:
+            return ctx[0]
     def parseList(self, ctx):
         return list(ctx)
     def parseDictionary(self, ctx):
         ret = dict(ctx)
-        """for i in range(1, ctx.count() + 1):
-            p = ctx.value(i)
-            ret[p[0]] = p[1]"""
-        if 'type' in ret:
-            if ret['type'] == "Feature":
-                if 'properties' in ret:
-                    properties = ret['properties']
-                    if 'STREET' in properties:
-                        if properties['STREET'] != "JEFFERSON":
-                            return None
-        return ret
-        """obj = ctx.value(1)
-        return obj"""
+        if ret.get('type') != 'Feature' or ret.get('properties', {}).get('STREET') == 'JEFFERSON':
+            return ret
     def parseDictionaryEntry(self, ctx):
         return tuple(ctx) if len(ctx) == 2 else (ctx[0], None)
 
-if __name__ == "__main__":
-    logging.basicConfig(filename="app.log", filemode='w', level=logging.DEBUG)
-
-    log_sink = LoggerManifold("")
+if __name__ == '__main__':
+    app_name = os.path.basename(sys.argv[0]).split('.')[0]
+    num_workers = int(sys.argv[1]) if len(sys.argv) > 1 else 1
+    logging.basicConfig(filename='debug_{}-p{}.log'.format(app_name, num_workers), filemode='w', level=logging.DEBUG)
+    log_sink = LoggerManifold('')
     log_sink.start()
 
-    worker_num = [1, 2, 4, 6, 8, 10, 12, 14, 16]
-    #worker_num = [2, 6, 10, 14, 18, 22, 26, 30, 34]
-    #worker_num = [34]
+    context = Context('../grammar/json2.cgr', num_workers)
+    input_path = '../datasets/citylots.json'
+    listener = JsonListener()
+    context.attach(listener)
+    context.start()
+    start_time = time.time()
+    result = context.parse(input_path)
+    end_time = time.time()
+    context.stop()
+    print(num_workers, end_time - start_time)
 
-    perf_log = open('perf.log', 'w')
-
-    for n in worker_num:
-        context = Context(r"../grammar/json2.cgr", n)
-        input_path = r"../datasets/citylots.json"
-        listener = JsonListener()
-        context.attach(listener)
-        context.start()
-        start_time = time.time()
-        result = context.parse(input_path)
-        end_time = time.time()
-        context.stop()
-        print("%d %f" % (n, end_time - start_time), file=perf_log)
-
-        result_log = open('result.log', 'w')
-        features_list = result['features']
-        for item in features_list:
-            if isinstance(item, dict) and len(item) > 0:
-                print(item, file=result_log)
+    if len(sys.argv) > 2 and sys.argv[2].lower() == 'debug':
+        with open('result_{}.log'.format(app_name), 'w') as dst:
+            features_list = result['features']
+            for item in features_list:
+                if isinstance(item, dict) and len(item) > 0:
+                    print(item, file=dst)
 
     log_sink.stop()
--- PythonApp1/bench_xml.py
+++ PythonApp1/bench_xml.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 
 from __future__ import print_function
+import sys
 import os
 import logging
 import multiprocessing as mp
 import time
 
-os.environ['CENTAURUS_DL_PATH'] = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "build")
+os.environ['CENTAURUS_DL_PATH'] = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'build')
 
 from Centaurus import *
 
@@ -14,20 +15,16 @@ logger = getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
 class XMLElement(object):
+    __slots__ = ('name', 'attr', 'body')
     def __init__(self, ctx):
         it = iter(ctx)
         self.name = next(it)
         self.attr = dict(next(it) for _ in range(len(ctx) - 2))
         self.body = next(it)
-    def get_content(self):
-        if len(self.body) == 0:
-            return ""
-        return self.body[0]
-    def get_child_content(self, name):
-        for item in self.body[1::2]:
-            if item.name == name:
-                return item.get_content()
-        return ""
+    def content(self):
+        return self.body[0] if self.body else ''
+    def children(self):
+        return (item for item in self.body if isinstance(item, XMLElement))
 
 class XMLListener(object):
     def parseName(self, ctx):
@@ -42,69 +39,55 @@ class XMLListener(object):
         return list(ctx)
     def parseElement(self, ctx):
         e = XMLElement(ctx)
-        if e.name == "article":
-            if e.get_child_content("year").strip() == "1990":
-                #logger.info("1990 article")
-                return e
-            else:
-                return None
-        elif e.name == "inproceedings" or e.name == "proceedings" or e.name == "book" or e.name == "incollection" or e.name == "phdthesis" or e.name == "mastersthesis" or e.name == "www":
-            return None
-        return e
+        if e.name == 'article':
+            for c in e.children():
+                if c.name == 'year' and c.content() == '1990':
+                    return e
+        elif e.name not in {'inproceedings', 'proceedings', 'book', 'incollection', 'phdthesis', 'mastersthesis', 'www'}:
+            return e
     def parseDocument(self, ctx):
         return ctx[0]
-    # def parseProlog(self, ctx):
-    #     return None
-    # def parseDocType(self, ctx):
-    #     return None
-    # def parseComment(self, ctx):
-    #     return None
     def defaultact(self, ctx):
-        return None
+        pass
 
-if __name__ == "__main__":
-    logging.basicConfig(filename="app.log", filemode='w', level=logging.DEBUG)
+def print_xml(doc, dstfile):
+    if isinstance(doc, XMLElement):
+        print('<' + doc.name, end='',  file=dstfile)
+        n = len(doc.body)
+        if n == 0:
+            print('>', end='', file=dstfile)
+        elif n > 1:
+            print('>', end='',  file=dstfile)
+            if doc.name in ('dblp', 'article'):
+                print(file=dstfile)
+            for i in range(n - 1):
+                print_xml(doc.body[i], dstfile)
+            print('</' + doc.body[n - 1] + '>', file=dstfile)
+        else:
+            assert False
+    elif isinstance(doc, str):
+        print(doc.replace('\n', ''), end='', file=dstfile)
 
-    log_sink = LoggerManifold("")
+if __name__ == '__main__':
+    app_name = os.path.basename(sys.argv[0]).split('.')[0]
+    num_workers = int(sys.argv[1]) if len(sys.argv) > 1 else 1
+    logging.basicConfig(filename='debug_{}-p{}.log'.format(app_name, num_workers), filemode='w', level=logging.DEBUG)
+    log_sink = LoggerManifold('')
     log_sink.start()
 
-    #worker_num = [2, 6, 10, 14, 18, 22, 26, 30, 34, 38, 42, 46]
-    #worker_num = [34]
-    #worker_num = [2, 6, 10, 14, 18, 22, 26, 30, 34]
-    worker_num = [1, 2, 4, 6, 8, 10, 12, 14, 16]
+    context = Context('../grammar/xml.cgr', num_workers)
+    input_path = '../datasets/dblp.xml'
+    listener = XMLListener()
+    context.attach(listener)
+    context.start()
+    start_time = time.time()
+    result = context.parse(input_path)
+    end_time = time.time()
+    context.stop()
+    print(num_workers, end_time - start_time)
 
-    perf_log = open('perf.log', 'w')
-
-    for n in worker_num:
-        context = Context(r"../grammar/xml.cgr", n)
-        input_path = r"../datasets/dblp.xml"
-        listener = XMLListener()
-        context.attach(listener)
-        context.start()
-        start_time = time.time()
-        result = context.parse(input_path)
-        end_time = time.time()
-        context.stop()
-        print("%d %f" % (n, end_time - start_time), file=perf_log)
-
-        result_log = open('result.log', 'w')
-        def print_xml(doc):
-            if isinstance(doc, XMLElement):
-                print('<' + doc.name, end='',  file=result_log)
-                n = len(doc.body)
-                if n == 0:
-                    print('>', end='', file=result_log)
-                elif n > 1:
-                    print('>', end='',  file=result_log)
-                    if doc.name in ('dblp', 'article'):
-                        print(file=result_log)
-                    for i in range(n - 1):
-                        print_xml(doc.body[i])
-                    print('</' + doc.body[n - 1] + '>', file=result_log)
-                else:
-                    assert False
-            elif isinstance(doc, str):
-                print(doc.replace('\n', ''), end='', file=result_log)
-        print_xml(result)
+    if len(sys.argv) > 2 and sys.argv[2].lower() == 'debug':
+        with open('result_{}.log'.format(app_name), 'w') as dst:
+            print_xml(result, dst)
 
     log_sink.stop()
