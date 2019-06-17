b24110209cb29b5273abd1674562fb68186c3d89
SATO Shigeyuki <sato@eidos.ic.i.u-tokyo.ac.jp>
2019-06-17 15:48:07 +0900

    correct and improve the script to run experiments in Python.

--- PythonApp1/run_pybench.sh
+++ PythonApp1/run_pybench.sh
@@ -3,13 +3,19 @@
 run='numactl --localalloc --cpunodebind=0'
 trials=15
 
+apps='bench_xml bench2'
+
+for app in ${apps}
+do
+    rm -fv ${app}.py
+done
 for p in 1 2 4 6 8 10 12 14 16
 do
-    for app in bench_xml bench2
+    for app in ${apps}
     do
         for _ in $(seq $trials)
         do
-            echo $run ${app}.py $p >> perf_${app}.log
+            $run ${app}.py $p >> perf_${app}.log
         done
     done
 done
