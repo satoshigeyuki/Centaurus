8b8a4ac7ebfc4a843c598670ce764db16f1848e1
SATO Shigeyuki <sato@eidos.ic.i.u-tokyo.ac.jp>
2019-08-12 02:20:26 +0900

    update the script to run C++ benchmarks.

---
+++ run_bench.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+run='numactl --localalloc --cpunodebind=0'
+trials=15
+
+apps='citylots dblp'
+
+for app in ${apps}
+do
+    rm -fv perf_${app}.log
+done
+
+for p in 1 $(seq 2 2 16)
+do
+    for app in ${apps}
+    do
+        for _ in $(seq $trials)
+        do
+            $run ./build/${app} $p >> perf_${app}.log
+        done
+    done
+done
+
+for p in $(seq 18 2 34)
+do
+    for app in ${apps}
+    do
+        for _ in $(seq $trials)
+        do
+            ./build/${app} $p >> perf_${app}.log
+        done
+    done
+done
