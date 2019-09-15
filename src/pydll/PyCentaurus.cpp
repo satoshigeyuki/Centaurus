3be5c1f5b2ef02ed600db91dacfbb977f0af4569
Hiroka IHARA <ihara@eidos.ic.i.u-tokyo.ac.jp>
2019-09-15 21:18:40 +0900

    Reorganization on VC++

--- src/pydll/PyCentaurus.cpp
+++ src/pydll/PyCentaurus.cpp
@@ -1,6 +1,7 @@
 ﻿// PyCentaurus.cpp : DLL アプリケーション用にエクスポートされる関数を定義します。
 //
 
+#include "stdafx.h"
 #include "Grammar.hpp"
 #include "Util.hpp"
 #ifdef PYCENTAURUS
--- tests/GrammarTest.cpp
+++ tests/GrammarTest.cpp
@@ -1,3 +1,4 @@
+#include "stdafx.h"
 #include "CppUnitTest.h"
 
 #include "NFA.hpp"
