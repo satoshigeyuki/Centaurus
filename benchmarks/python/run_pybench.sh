9568614a981745a82c4681c925a8e1fe032fd6b0
SATO Shigeyuki <sato@eidos.ic.i.u-tokyo.ac.jp>
2019-09-29 18:45:53 +0900

    update a script for running experiments in Python on spica (changed from comet).

--- benchmarks/python/run_pybench.sh
+++ benchmarks/python/run_pybench.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-run='numactl --localalloc --cpunodebind=0'
 trials=15
 
 if [ $# -eq 0 ]
@@ -14,7 +13,9 @@ for app in ${apps}
 do
     rm -fv perf_${app}.log
 done
-for p in 1 $(seq 2 2 16)
+
+run='numactl --localalloc --cpunodebind=0'
+for p in 1 $(seq 2 2 22)
 do
     for app in ${apps}
     do
@@ -24,13 +25,25 @@ do
         done
     done
 done
-for p in $(seq 18 2 34)
+run='numactl --localalloc --cpunodebind=0-1'
+for p in $(seq 24 2 46)
 do
     for app in ${apps}
     do
         for _ in $(seq $trials)
         do
-            ./${app}.py $p >> perf_${app}.log
+            $run ./${app}.py $p >> perf_${app}.log
+        done
+    done
+done
+run='numactl --localalloc --cpunodebind=0-2'
+for p in $(seq 48 2 70)
+do
+    for app in ${apps}
+    do
+        for _ in $(seq $trials)
+        do
+            $run ./${app}.py $p >> perf_${app}.log
         done
     done
 done
