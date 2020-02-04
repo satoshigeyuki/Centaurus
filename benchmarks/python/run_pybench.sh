b8a7b3cfdbbb32bbd25aed588cc991e2579f1258
SATO Shigeyuki <sato@eidos.ic.i.u-tokyo.ac.jp>
2020-02-04 20:40:04 +0900

    change to run the experiments in Python without --cpunodebind of numactl.

--- benchmarks/python/run_pybench.sh
+++ benchmarks/python/run_pybench.sh
@@ -14,36 +14,18 @@ do
     rm -fv perf_${app}.log
 done
 
-run='numactl --localalloc --cpunodebind=0'
-for p in 1 $(seq 2 2 22)
-do
-    for app in ${apps}
+run_apps() {
+    for p in $*
     do
-        for _ in $(seq $trials)
+        for app in ${apps}
         do
-            $run ./${app}.py $p >> perf_${app}.log
+            for _ in $(seq $trials)
+            do
+                $run ./${app}.py $p >> perf_${app}.log
+            done
         done
     done
-done
-run='numactl --localalloc --cpunodebind=0-1'
-for p in $(seq 24 2 46)
-do
-    for app in ${apps}
-    do
-        for _ in $(seq $trials)
-        do
-            $run ./${app}.py $p >> perf_${app}.log
-        done
-    done
-done
-run='numactl --localalloc --cpunodebind=0-2'
-for p in $(seq 48 2 70)
-do
-    for app in ${apps}
-    do
-        for _ in $(seq $trials)
-        do
-            $run ./${app}.py $p >> perf_${app}.log
-        done
-    done
-done
+}
+
+run='numactl --localalloc'
+run_apps 1 $(seq 2 2 94)
